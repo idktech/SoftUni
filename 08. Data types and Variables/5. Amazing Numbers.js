b13f608a8c686e12859a9c48ced839b40f5cffa3
3vo <idk@gmx.com>
2023-05-30 20:23:15 +0300

    solved amazing numbers task

--- 08. Data types and Variables/5. Amazing Numbers.js
+++ 08. Data types and Variables/5. Amazing Numbers.js
@@ -5,18 +5,21 @@
 // Examples for amazing numbers are 1233 (1 + 2 + 3 + 3 = 9), 583472 (5 + 8 + 3 + 4 + 7 + 2 = 29)
 
 
-function amazingNums(numAsTex){
-    let sum = 0;
+function checkAmazingNumber(number) {
+    // Convert the number to a string to iterate over its digits
+    let numberString = number.toString();
 
-    for (let index = 0; index < numAsTex.length; index++) {
-        sum += Number(numAsTex[i])
-        console.log(numAsTex[i]);
+    // Calculate the sum of the digits
+    let sum = 0;
+    for (let i = 0; i < numberString.length; i++) {
+        sum += Number(numberString[i]);
     }
 
-    let isAmazing = sum.toString().includes("9") ? "True" : "False"
+    // Check if the sum of the digits is equal to 9
+    let isAmazing = sum.toString().includes("9") ? "True" : "False";
 
-    console.log(`${numAsTex} Amazing? ${isAmazing}`);
+    // Print the result
+    console.log(`${number} Amazing? ${isAmazing}`);
 }
 
-amazingNums(1233)
-
+checkAmazingNumber(1233)
