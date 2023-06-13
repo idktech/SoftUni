c4be26fa99317e9ce18133707ae5a0fa6fd4dd6d
3vo <idk@gmx.com>
2023-06-13 16:01:42 +0300

    task 03 solved

--- 16. Arrays Advanced/03. First and Last K Numbers.js
+++ 16. Arrays Advanced/03. First and Last K Numbers.js
@@ -1,1 +1,12 @@
-function firstAndLast() {}
+function firstAndLastKNumbers(data) {
+    let k = data.shift();
+    let firstK = data.slice(0, k)
+    let lastK = data.slice(k *  -1);
+
+    console.log(firstK.join(" "));
+    console.log((lastK).join(" "));
+
+}
+
+firstAndLastKNumbers([3,
+    6, 7, 8, 9])
