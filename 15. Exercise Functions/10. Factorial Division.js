4dc2669c9c370d2c7e8ce8c390db520f5fbcc52f
idktech <idk@gmx.com>
2023-06-13 12:09:17 +0300

    solved factorial second option

--- 15. Exercise Functions/10. Factorial Division.js
+++ 15. Exercise Functions/10. Factorial Division.js
@@ -1,15 +1,31 @@
+// function factorialDivision(num1, num2) {
+//   function factorial(n) {
+//     if (n === 0) {
+//       return 1;
+//     } else {
+//       return n * factorial(n - 1);
+//     }
+//   }
+
+//   let factorialNum1 = factorial(num1);
+//   let factorialNum2 = factorial(num2);
+//   let divisionResult = factorialNum1 / factorialNum2;
+
+//   console.log(divisionResult.toFixed(2));
+// }
+
 function factorialDivision(num1, num2) {
   function factorial(n) {
-    if (n === 0) {
-      return 1;
-    } else {
-      return n * factorial(n - 1);
+    let result = 1;
+    for (let i = 2; i <= n; i++) {
+      result *= i;
     }
+    return result;
   }
 
-  let factorialNum1 = factorial(num1);
-  let factorialNum2 = factorial(num2);
-  let divisionResult = factorialNum1 / factorialNum2;
+  const factorialNum1 = factorial(num1);
+  const factorialNum2 = factorial(num2);
+  const divisionResult = factorialNum1 / factorialNum2;
 
   console.log(divisionResult.toFixed(2));
 }
