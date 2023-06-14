2af1c5b4b43af388f9545fcac8b4f1e379500f6e
3vo <idk@gmx.com>
2023-06-14 08:32:07 +0300

    solved 04 task Last K Numbers Sequance

--- 16. Arrays Advanced/04. Last K Numbers Sequance.js
+++ 16. Arrays Advanced/04. Last K Numbers Sequance.js
@@ -1,3 +1,12 @@
-function lastNumsSeq(data){
-    
+function generateSequence(n, k) {
+    const sequence = [1]; // start sequence with first element equal to 1
+
+    // loop through the remaining n-1 elements and generate the sequence
+    for (let i = 1; i < n; i++) {
+        const sum = sequence.slice(-k).reduce((acc, curr) => acc + curr, 0); // get the sum of previous k elements
+        sequence.push(sum); // add the sum to the sequence
+    }
+    console.log(sequence.join(" ")); // print the sequence with spaces between elements
 }
+
+generateSequence(8, 2)
