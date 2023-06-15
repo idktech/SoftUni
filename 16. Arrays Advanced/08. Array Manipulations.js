0bf0dafc78604b5e6147074b312817ce39cff4e8
3vo <idk@gmx.com>
2023-06-15 09:05:24 +0300

    started not finieshed arrManipolation task

---
+++ 16. Arrays Advanced/08. Array Manipulations.js
@@ -0,0 +1,15 @@
+function arrManipulation(arr) {
+
+     
+}
+
+arrManipulation(['4 19 2 53 6 43',
+    'Add 3',
+    'Remove 2',
+    'RemoveAt 1',
+    'Insert 8 3'])
+arrManipulation(['6 12 2 65 6 42',
+'Add 8',
+'Remove 12',
+'RemoveAt 3',
+'Insert 6 2'])
