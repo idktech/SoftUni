262a59e94d633afdc22ce17cc23a8de00e6ff052
3vo <idk@gmx.com>
2023-06-15 10:06:12 +0300

    edited sortAnArray and upload bombNums task

--- 18. Exercise Arrays Advanced/05. Sort an Array by 2 Criteria.js
+++ 18. Exercise Arrays Advanced/05. Sort an Array by 2 Criteria.js
@@ -1,17 +1,22 @@
 function arrayOfStrings(arrays) {
 
-    let sorted = arrays.sort((a, b) => {
-        if (a.length !== b.length) {
-            return a.length - b.length;
-        }
-        return a.localeCompare(b);
+    //* ? First method
+    // let sorted = arrays.sort((a, b) => {
+    //     if (a.length !== b.length) {
+    //         return a.length - b.length;
+    //     }
+    //     return a.localeCompare(b);
+    // })
+    //* ? Second method
+    let sortedArray = arrays.sort((a, b) => {
+        return a.length - b.length || a.localeCompare(b);
     })
 
-    console.log(sorted.join("\n"));
+    console.log(sortedArray.join("\n"));
 }
 
 arrayOfStrings(['Isacc',
-'Theodor',
-'Jack',
-'Harrison',
-'George'])
+    'Theodor',
+    'Jack',
+    'Harrison',
+    'George'])
